f1a7a54368d674d95658bb821e9e5c91c4119216
takahiro <suzuki-takahiro@noa-sol.co.jp>
2020-10-12 20:33:44 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -13,16 +13,33 @@ import { render } from '@testing-library/react';
 // }
 
 const App=()=>{
-  return(<div>
-  <Cat />
-  <Cat />
-  <Cat />
-  <Cat />
+  const profiles=[
+    {
+      name:"Taro",age:10
+    },
+    {
+      name:"Hanako",age:5
+    },
+    {
+      name:"Noname"
+    }
+  ]
+  return(
+  <div>
+    {
+      profiles.map((profile,index)=>{
+        return <User name={profile.name} age={profile.age} key={index}/>
+      })
+    }
   </div>)
 }
 
-const Cat=()=>{
-  return<div>Hi!</div>
+const User=(props)=>{
+return<div>Hi,I am{props.name},and {props.age} years old!</div>
+}
+
+User.defaultProps={
+  age:1
 }
 
 export default App;
